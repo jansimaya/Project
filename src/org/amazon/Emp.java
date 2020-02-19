c7c4875276a8516093a5cba8b453ed596b7709ab
Ravi <jansirar@amazon.com>
2020-02-19 16:38:58 +0530

    final project

---
+++ src/org/amazon/Emp.java
@@ -0,0 +1,40 @@
+package org.amazon;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Emp {
+	static WebDriver driver;
+	@BeforeClass
+	public void beforeclass() {
+		System.setProperty("Webdriver.chrome.driver", "C:\\Users\\jansirar\\eclipse-workspace\\Testng\\dri\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+	}
+	@AfterClass
+	public void afterclass() {
+		driver.quit();
+	}
+	
+	
+	@Test
+	public void test() {
+		WebElement txtuser = driver.findElement(By.id("email"));
+		txtuser.sendKeys("jansi");
+		WebElement txtpass = driver.findElement(By.id("pass"));
+		txtpass.sendKeys("jansi2019");
+	}
+	@Test
+	public void abc() {
+		WebElement btn = driver.findElement(By.id("login"));
+		btn.click();
+	}
+
+
+
+}
